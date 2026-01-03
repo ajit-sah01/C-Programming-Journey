570b702fe75dd37b431340289882f0318327cb48
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-03 08:09:36 +0530

    Add files via upload
    Introduces the basic structure of a C program by printing “Hello, World!” to understand compilation, execution, and standard output.

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   // Standard input-output header
+
+int main() {
+    // Print Hello World message
+    printf("Hello, World!\n");
+
+    // Indicate successful program execution
+    return 0;
+}
